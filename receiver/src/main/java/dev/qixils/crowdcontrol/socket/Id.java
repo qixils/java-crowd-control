5d3836c77607195906d42f5362c8b3cb3120f9c9
Lexi Larkin <lexi@qixils.dev>
2023-06-16 12:53:51 -0400

    fix: Id lacking equals/hashCode

--- receiver/src/main/java/dev/qixils/crowdcontrol/socket/Id.java
+++ receiver/src/main/java/dev/qixils/crowdcontrol/socket/Id.java
@@ -5,6 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
+import java.util.Objects;
+
+import static dev.qixils.crowdcontrol.util.StringUtils.repr;
 
 class Id {
 	public final @NotNull String id;
@@ -13,5 +16,26 @@ class Id {
 	public Id(@NotNull String id, @Nullable IdType type) {
 		this.id = id.toLowerCase(Locale.ENGLISH);
 		this.type = ExceptionUtil.validateNotNullElse(type, IdType.EFFECT);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Id other = (Id) o;
+		return id.equals(other.id) && type == other.type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, type);
+	}
+
+	@Override
+	public String toString() {
+		return "Id{" +
+				"id=" + repr(id) +
+				", type=" + type +
+				'}';
 	}
 }
